b6aa526c95f0e4093c900db587a589fa85dcf1b1
AlexandrLi <muzikant1990@gmail.com>
2016-02-24 04:40:12 +0600

    Refactor Runner class

--- src/main/java/com/epam/alexandrli/textparser/Runner.java
+++ src/main/java/com/epam/alexandrli/textparser/Runner.java
@@ -1,20 +1,12 @@
 package com.epam.alexandrli.textparser;
 
 
-import static com.epam.alexandrli.textparser.CharLeaf.CharCache;
-
 public class Runner {
     public static void main(String[] args) {
         TextFactory textFactory = new TextFactory();
         String textFromFile = textFactory.createTextFromFile("text.txt");
-        System.out.println(textFromFile);
-
-        CharLeaf char1 = CharCache.valueOf('A');
-        CharLeaf char2 = CharCache.valueOf('B');
-        CharLeaf char3 = CharCache.valueOf('C');
-        CharLeaf char4 = CharCache.valueOf('A');
-        CharLeaf char5 = CharCache.valueOf('C');
-
+        TextParser textParser = new TextParser();
+        CompositeText compositeText = textParser.parseText(textFromFile);
 
     }
 }
