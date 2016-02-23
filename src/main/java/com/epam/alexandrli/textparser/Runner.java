7a6d06560f37c2859bd679b64898b8392365ae34
AlexandrLi <muzikant1990@gmail.com>
2016-02-23 16:49:35 +0600

    use correct Object initialization

--- src/main/java/com/epam/alexandrli/textparser/Runner.java
+++ src/main/java/com/epam/alexandrli/textparser/Runner.java
@@ -1,11 +1,20 @@
 package com.epam.alexandrli.textparser;
 
 
+import static com.epam.alexandrli.textparser.CharLeaf.CharCache;
+
 public class Runner {
     public static void main(String[] args) {
         TextFactory textFactory = new TextFactory();
         String textFromFile = textFactory.createTextFromFile("text.txt");
         System.out.println(textFromFile);
+
+        CharLeaf char1 = CharCache.valueOf('A');
+        CharLeaf char2 = CharCache.valueOf('B');
+        CharLeaf char3 = CharCache.valueOf('C');
+        CharLeaf char4 = CharCache.valueOf('A');
+        CharLeaf char5 = CharCache.valueOf('C');
+
 
     }
 }
