44a84aa817f31b4405452db3a82b242e917172d5
AlexandrLi <muzikant1990@gmail.com>
2016-02-23 20:53:33 +0600

    add ValueOf method to CharLeaf to get cached values

--- src/main/java/com/epam/alexandrli/textparser/CharLeaf.java
+++ src/main/java/com/epam/alexandrli/textparser/CharLeaf.java
@@ -2,38 +2,31 @@ package com.epam.alexandrli.textparser;
 
 import java.util.ArrayList;
 
-public class CharLeaf implements Printable {
+public class CharLeaf implements Component {
 
-    private char value;
-
-    public CharLeaf() {
-    }
+    private final char value;
 
     private CharLeaf(char value) {
         this.value = value;
     }
 
-    private CharLeaf valueOf(char value) {
-        return CharCache.valueOf(value);
+    @Override
+    public String toPlainString() {
+        return String.valueOf(value);
     }
 
-    @Override
-    public void print() {
-        System.out.println(value);
+    public static CharLeaf valueOf(char value) {
+        return CharCache.getCachedValue(value);
     }
 
     public char getValue() {
         return value;
     }
 
-    public void setValue(char value) {
-        this.value = value;
-    }
-
-    public static class CharCache {
+    private static class CharCache {
         private static ArrayList<CharLeaf> charCache = new ArrayList<>();
 
-        public static CharLeaf valueOf(char value) {
+        public static CharLeaf getCachedValue(char value) {
             return charCache.get(findCachedValueIndex(value));
         }
 
