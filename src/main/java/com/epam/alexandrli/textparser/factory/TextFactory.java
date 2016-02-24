2948065cd7b9de5983539a012e3975e5fe63e9c3
AlexandrLi <muzikant1990@gmail.com>
2016-02-25 03:05:02 +0600

    Refactor createTextFromFile method. Move to factory package

---
+++ src/main/java/com/epam/alexandrli/textparser/factory/TextFactory.java
@@ -0,0 +1,13 @@
+package com.epam.alexandrli.textparser.factory;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class TextFactory {
+
+    public String createTextFromFile(String fileName) {
+        InputStream in = TextFactory.class.getClassLoader().getResourceAsStream("text/" + fileName);
+        return new Scanner(in).useDelimiter("\\A").next();
+    }
+
+}
