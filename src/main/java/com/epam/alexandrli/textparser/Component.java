7ab0efdffb7f841193109fbdd16df242030a0a5c
AlexandrLi <muzikant1990@gmail.com>
2016-02-25 03:08:36 +0600

    Change toPlainString method signature

--- src/main/java/com/epam/alexandrli/textparser/Component.java
+++
@@ -1,7 +0,0 @@
-package com.epam.alexandrli.textparser;
-
-public interface Component {
-
-    String toPlainString();
-
-}
---
+++ src/main/java/com/epam/alexandrli/textparser/entity/Component.java
@@ -0,0 +1,7 @@
+package com.epam.alexandrli.textparser.entity;
+
+public interface Component {
+
+    String toPlainString(StringBuilder sb);
+
+}
