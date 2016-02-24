12c78bd22e389a6b4a2605252b171d59c5e8ae80
AlexandrLi <muzikant1990@gmail.com>
2016-02-25 03:11:52 +0600

    Refactor App demonstration

--- src/main/java/com/epam/alexandrli/textparser/Runner.java
+++ src/main/java/com/epam/alexandrli/textparser/Runner.java
@@ -1,12 +1,17 @@
 package com.epam.alexandrli.textparser;
 
 
+import com.epam.alexandrli.textparser.entity.CompositeText;
+import com.epam.alexandrli.textparser.factory.TextFactory;
+import com.epam.alexandrli.textparser.parser.TextParser;
+
 public class Runner {
     public static void main(String[] args) {
         TextFactory textFactory = new TextFactory();
         String textFromFile = textFactory.createTextFromFile("text.txt");
         TextParser textParser = new TextParser();
         CompositeText compositeText = textParser.parseText(textFromFile);
-
+        String s = compositeText.toPlainString(new StringBuilder());
+        System.out.println(s);
     }
 }
