45cf320cad4339d5b68bcc5ba9bbcf256c2adba8
AlexandrLi <muzikant1990@gmail.com>
2016-02-26 00:47:05 +0600

    Add logging in Runner class

--- src/main/java/com/epam/alexandrli/textparser/Runner.java
+++ src/main/java/com/epam/alexandrli/textparser/Runner.java
@@ -2,16 +2,23 @@ package com.epam.alexandrli.textparser;
 
 
 import com.epam.alexandrli.textparser.entity.CompositeText;
+import com.epam.alexandrli.textparser.factory.NewLoggerFactory;
 import com.epam.alexandrli.textparser.factory.TextFactory;
 import com.epam.alexandrli.textparser.parser.TextParser;
+import org.slf4j.Logger;
 
 public class Runner {
+    final static Logger logger = NewLoggerFactory.createLoggerWithConfigFromFile(Runner.class, "logback.xml");
+
     public static void main(String[] args) {
+        logger.info("App started!");
         TextFactory textFactory = new TextFactory();
         String textFromFile = textFactory.createTextFromFile("text.txt");
+        logger.info("Text read from file");
         TextParser textParser = new TextParser();
+        logger.info("Start text parsing");
         CompositeText compositeText = textParser.parseText(textFromFile);
+        logger.info("Text has been parsed successfully");
         String s = compositeText.toPlainString(new StringBuilder());
-        System.out.println(s);
     }
 }
