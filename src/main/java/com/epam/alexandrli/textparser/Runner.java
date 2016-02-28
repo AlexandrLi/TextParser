a183514b77587a0235293aa00d41d363e6316178
AlexandrLi <muzikant1990@gmail.com>
2016-02-28 22:06:18 +0600

    Use LoggerFactory to init logger

--- src/main/java/com/epam/alexandrli/textparser/Runner.java
+++ src/main/java/com/epam/alexandrli/textparser/Runner.java
@@ -2,22 +2,23 @@ package com.epam.alexandrli.textparser;
 
 
 import com.epam.alexandrli.textparser.entity.CompositeText;
-import com.epam.alexandrli.textparser.factory.NewLoggerFactory;
-import com.epam.alexandrli.textparser.factory.TextFactory;
+import com.epam.alexandrli.textparser.factory.TextReader;
 import com.epam.alexandrli.textparser.parser.TextParser;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Runner {
-    final static Logger logger = NewLoggerFactory.createLoggerWithConfigFromFile(Runner.class, "logback.xml");
+    final static Logger logger = LoggerFactory.getLogger(Runner.class);
 
     public static void main(String[] args) {
         logger.info("App started!");
-        TextFactory textFactory = new TextFactory();
-        String textFromFile = textFactory.createTextFromFile("text.txt");
+        TextReader textReader = new TextReader();
+        String textFromFile = textReader.createTextFromFile("text.txt");
         logger.info("Text read from file");
         TextParser textParser = new TextParser();
         logger.info("Start text parsing");
         CompositeText compositeText = textParser.parseText(textFromFile);
         logger.info("Text has been parsed successfully");
+        System.out.println();
     }
 }
