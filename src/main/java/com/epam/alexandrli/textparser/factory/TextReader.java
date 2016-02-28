10041f86e464416fda2149e57a2a77fc1e4c82a2
AlexandrLi <muzikant1990@gmail.com>
2016-02-28 22:01:06 +0600

    rename TextFactory to TextReader

--- src/main/java/com/epam/alexandrli/textparser/factory/TextReader.java
+++ src/main/java/com/epam/alexandrli/textparser/factory/TextReader.java
@@ -2,10 +2,10 @@ package com.epam.alexandrli.textparser.factory;
 import java.io.InputStream;
 import java.util.Scanner;
 
-public class TextFactory {
+public class TextReader {
 
     public String createTextFromFile(String fileName) {
-        InputStream in = TextFactory.class.getClassLoader().getResourceAsStream("text/" + fileName);
+        InputStream in = TextReader.class.getClassLoader().getResourceAsStream("text/" + fileName);
         return new Scanner(in).useDelimiter("\\A").next();
     }
 
