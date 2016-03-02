a07738a83e50d05fecf0b616c81fd9bad96853aa
AlexandrLi <muzikant1990@gmail.com>
2016-03-02 14:14:42 +0600

    Remove inner class RegexType and use it's variables in TextParser class

--- src/main/java/com/epam/alexandrli/textparser/parser/TextParser.java
+++ src/main/java/com/epam/alexandrli/textparser/parser/TextParser.java
@@ -5,79 +5,69 @@ import com.epam.alexandrli.textparser.entity.CompositeText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
+import static com.epam.alexandrli.textparser.PropertyManager.REGEX_PROPERTIES;
+import static com.epam.alexandrli.textparser.PropertyManager.getProperties;
+import static com.epam.alexandrli.textparser.entity.CompositeText.Type;
+
 public class TextParser {
+    private static final String PARAGRAPH = "paragraph";
+    private static final String SENTENCE = "sentence";
+    private static final String WORD = "word";
     private final static Logger logger = LoggerFactory.getLogger(TextParser.class);
+    private Properties regexProperties = getProperties(REGEX_PROPERTIES);
 
     public TextParser() {
     }
 
     public CompositeText parseText(String stringText) {
         CompositeText text = new CompositeText();
-        text.setType(CompositeText.Type.TEXT);
-        String[] textParagraphs = stringText.split(RegexType.getRegex(RegexType.PARAGRAPH));
+        text.setType(Type.TEXT);
+        String[] textParagraphs = stringText.split(regexProperties.getProperty(PARAGRAPH));
         for (String textParagraph : textParagraphs) {
             text.add(parseParagraphs(textParagraph));
         }
-        logger.debug("The result of {} parsing is: {}", CompositeText.Type.TEXT, text.toString());
+        logger.debug("The result of {} parsing is: {}", Type.TEXT, text.toString());
         return text;
     }
 
     public CompositeText parseParagraphs(String text) {
-        logger.info("Parse {}", CompositeText.Type.PARAGRAPH);
+        logger.info("Parse {}", Type.PARAGRAPH);
         CompositeText paragraph = new CompositeText();
-        paragraph.setType(CompositeText.Type.PARAGRAPH);
-        String[] textSentences = text.split(RegexType.getRegex(RegexType.SENTENCE));
+        paragraph.setType(Type.PARAGRAPH);
+        String[] textSentences = text.split(regexProperties.getProperty(SENTENCE));
         for (String textSentence : textSentences) {
             paragraph.add(parseSentence(textSentence));
         }
-        logger.debug("The result of {} parsing is: {}", CompositeText.Type.PARAGRAPH, paragraph.toString());
+        logger.debug("The result of {} parsing is: {}", Type.PARAGRAPH, paragraph.toString());
         return paragraph;
     }
 
     public CompositeText parseSentence(String text) {
-        logger.info("Parse {}", CompositeText.Type.SENTENCE);
+        logger.info("Parse {}", Type.SENTENCE);
         CompositeText sentence = new CompositeText();
-        sentence.setType(CompositeText.Type.SENTENCE);
-        String[] textWords = text.split(RegexType.getRegex(RegexType.WORD));
+        sentence.setType(Type.SENTENCE);
+        String[] textWords = text.split(regexProperties.getProperty(WORD));
         for (String textWord : textWords) {
             sentence.add(parseWord(textWord));
         }
-        logger.debug("The result of {} parsing is: {}", CompositeText.Type.SENTENCE, sentence.toString());
+        logger.debug("The result of {} parsing is: {}", Type.SENTENCE, sentence.toString());
         return sentence;
     }
 
     public CompositeText parseWord(String text) {
-        logger.info("Parse {}", CompositeText.Type.WORD);
+        logger.info("Parse {}", Type.WORD);
         CompositeText word = new CompositeText();
-        word.setType(CompositeText.Type.WORD);
+        word.setType(Type.WORD);
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
             CharLeaf symbol = CharLeaf.valueOf(text.charAt(i));
             logger.debug("Parsed symbol is: {}", symbol.toString());
             word.add(symbol);
         }
-        logger.debug("The result of {} parsing is: {}", CompositeText.Type.WORD, word.toString());
+        logger.debug("The result of {} parsing is: {}", Type.WORD, word.toString());
         return word;
     }
 
-    private static class RegexType {
-        private static final String WORD = "word";
-        private static final String SENTENCE = "sentence";
-        private static final String PARAGRAPH = "paragraph";
-
-        private static String getRegex(String propertyKey) {
-            InputStream in = TextParser.class.getClassLoader().getResourceAsStream("properties/regexType.properties");
-            Properties regexProperties = new Properties();
-            try {
-                regexProperties.load(in);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return regexProperties.getProperty(propertyKey);
-        }
-    }
 }
