78c816776d00d731ae48cd094a06e8be2c1b19d3
Alexandr.li <muzikant1990@gmail.com>
2016-03-07 15:48:48 +0600

    Add Iterator implementations. Realize DeepIterator class

--- src/main/java/com/epam/alexandrli/textparser/entity/CompositeText.java
+++ src/main/java/com/epam/alexandrli/textparser/entity/CompositeText.java
@@ -1,14 +1,15 @@
 package com.epam.alexandrli.textparser.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class CompositeText implements Component {
+public class CompositeText implements Component, Iterable<Component> {
+    private static List<Type> typeHierarchy = new ArrayList<>();
     private List<Component> components = new ArrayList<>();
     private Type type;
 
 
     public CompositeText() {
+        Collections.addAll(typeHierarchy, Type.values());
     }
 
     public List<Component> getComponents() {
@@ -55,7 +56,77 @@ public class CompositeText implements Component {
         return components.get(index);
     }
 
+    public boolean isNestedType(Type type) {
+        for (int i = typeHierarchy.indexOf(type); i < typeHierarchy.size(); i++) {
+            if (type == typeHierarchy.get(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isNearestNestedType(Type type) {
+        return (typeHierarchy.indexOf(type) - typeHierarchy.indexOf(this.type)) == 1;
+    }
+
+    public Iterator<Component> iterator(Type type) {
+        if (isNearestNestedType(type)) {
+            return iterator();
+        } else {
+            if (isNestedType(type)) {
+                return new DeepIterator(type);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Component> iterator() {
+        return components.iterator();
+    }
+
     public enum Type {
-        WORD, SENTENCE, PARAGRAPH, TEXT
+        TEXT, PARAGRAPH, SENTENCE, SENTENCE_PART
+    }
+
+    private class DeepIterator implements Iterator<Component> {
+        Stack<Iterator<Component>> stack = new Stack<>();
+        Type deepType;
+
+        public DeepIterator(Type type) {
+            stack.push(components.iterator());
+            deepType = type;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            Iterator<Component> componentIterator = stack.peek();
+            if (componentIterator.hasNext()) {
+                return true;
+            }
+            stack.pop();
+            return hasNext();
+        }
+
+        @Override
+        public Component next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Iterator<Component> componentIterator = stack.peek();
+            Component component = componentIterator.next();
+            if (component instanceof CompositeText) {
+                if (((CompositeText) component).getType() == deepType) {
+                    return component;
+                }
+                stack.push(((CompositeText) component).iterator());
+            }
+            return next();
+        }
     }
 }
