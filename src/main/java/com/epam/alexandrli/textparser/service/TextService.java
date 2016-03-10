f17e5051fe0f77f30f0d364a865d39c366c01da7
Alexandr.li <muzikant1990@gmail.com>
2016-03-10 20:23:03 +0600

    Optimize methods in class.

--- src/main/java/com/epam/alexandrli/textparser/service/TextService.java
+++ src/main/java/com/epam/alexandrli/textparser/service/TextService.java
@@ -1,12 +1,8 @@
 package com.epam.alexandrli.textparser.service;
 
-import com.epam.alexandrli.textparser.entity.Component;
 import com.epam.alexandrli.textparser.entity.CompositeText;
-import com.sun.corba.se.impl.io.TypeMismatchException;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import static com.epam.alexandrli.textparser.entity.CompositeText.Type;
@@ -14,28 +10,10 @@ import static com.epam.alexandrli.textparser.entity.CompositeText.Type;
 public class TextService {
     public static final Comparator<CompositeText> COMPONENTS_COUNT_ORDER = new ComponentsCountComparator();
 
-    public List<CompositeText> sortSentencesByWordsCount(CompositeText text) {
-        List<CompositeText> sentences = text.getCompositeList(Type.SENTENCE);
-        sentences.sort(COMPONENTS_COUNT_ORDER);
-        return sentences;
-    }
-
-    public List<Component> getAllWordsFromSentence(CompositeText text) {
-        if (text.getType() != Type.SENTENCE) {
-            throw new TypeMismatchException("CompositeText type must be SENTENCE");
-        }
-        Iterator<Component> iterator = text.iterator();
-        List<Component> words = new ArrayList<>();
-        Component component;
-        while (iterator.hasNext()) {
-            component = iterator.next();
-            if (component instanceof CompositeText) {
-                if (((CompositeText) component).getType() == Type.WORD) {
-                    words.add(component);
-                }
-            }
-        }
-        return words;
+    public List<CompositeText> sortComponentsByCondition(CompositeText text, Type componentType, Comparator<CompositeText> condition) {
+        List<CompositeText> compositeList = text.getCompositeList(componentType);
+        compositeList.sort(condition);
+        return compositeList;
     }
 
     private static class ComponentsCountComparator implements Comparator<CompositeText> {
